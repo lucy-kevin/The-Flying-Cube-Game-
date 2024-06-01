9e12e31bfe96b7ea95ca16b15116a95d6d7d4ad6
lucy-kevin <kayikayikevin7@gmail.com>
2024-06-01 17:31:19 +0300

    This fixes #2

--- game/src/mainmenu.js
+++ game/src/mainmenu.js
@@ -91,6 +91,7 @@ class MainMenu extends GameObject {
 		// Create a Text Block that can display the current score
 		this.welcomeText = new BABYLON.GUI.TextBlock();
 		this.welcomeText.text = "Flying Cube Game!";
+		this.welcomeText.fontFamily = "'Courier New', Courier, monospace"
 		this.welcomeText.fontFamily = "Impact";
 		this.welcomeText.color = "white";
 		this.welcomeText.fontSize = 72;
@@ -101,7 +102,7 @@ class MainMenu extends GameObject {
 
 		this.greetingText = new BABYLON.GUI.TextBlock();
 		this.greetingText.text = greeting;
-		this.greetingText.fontFamily = "Impact";
+		this.greetingText.fontFamily = "'Courier New', Courier, monospace";
 		this.greetingText.color = "white";
 		this.greetingText.fontSize = 28;
 		this.greetingText.verticalAlignment = BABYLON.GUI.TextBlock.VERTICAL_ALIGNMENT_TOP;
@@ -111,7 +112,7 @@ class MainMenu extends GameObject {
 
 		this.instructionsText = new BABYLON.GUI.TextBlock();
 		this.instructionsText.text = "press any key to play";
-		this.instructionsText.fontFamily = "Impact";
+		this.instructionsText.fontFamily = "'Courier New', Courier, monospace";
 		this.instructionsText.color = "#aafffa";
 		this.instructionsText.fontSize = 32;
 		this.instructionsText.verticalAlignment = BABYLON.GUI.TextBlock.VERTICAL_ALIGNMENT_TOP;
